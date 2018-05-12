6969a6dcd7a48114a9681542ade3e58fc920e7f3
yhy <570805251@qq.com>
2018-05-12 21:12:10 +0800

    feat:联系fromat

--- str_format.py
+++ str_format.py
@@ -3,3 +3,8 @@ name = "Peter"
 
 print("{0} was {1} year old when he wrote this book".format(name, age))
 print("Why is {0} playing with that python".format(name))
+print("{0:.3f}".format(1.0 / 3))
+print("{0:=^10}".format("hello"))
+print("{name} wrote {book}".format(name='peter', book='A Byte for python'))
+print("abc", end='')
+print("abcd", end='')
