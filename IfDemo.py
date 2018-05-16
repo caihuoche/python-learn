0192445ad63244369d428cc3017086a912e712f2
yhy <570805251@qq.com>
2018-05-16 18:58:39 +0800

    feat: 测试if 和 变量

---
+++ IfDemo.py
@@ -0,0 +1,12 @@
+length = 2
+hi = 10
+if length > hi:
+    print("length 是大于hi的")
+elif length < hi:
+    print("length 是小于hi的")
+else:
+    print("length 等于hi")
+
+print("结束")
+if True:
+    print("this is true")
---
+++ VariableDemo.py
@@ -0,0 +1,9 @@
+i = 5
+print(i)
+i = i + 1
+print(i)
+s = ''' this is test .dad
+ ad'''
+print(s)
+2 + 8
+print(2 + 8)
